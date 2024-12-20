867dea459752a93f8c6e03230ffe20cf70545c26
ritik21311 <65362957+ritik21311@users.noreply.github.com>
2024-12-20 08:35:27 +0000

    datatypes summary

---
+++ .devcontainer/js_basics/compar.js
@@ -0,0 +1,26 @@
+// console.log("2" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// confusion creation
+
+// === 
+
+// The strict equality operator (===) behaves identically to the abstract equality operator (==) except no type conversion is done, 
+// and the types must be the same to be considered equal.
+
+// Reference: JavaScript Tutorial: Comparison Operators
+
+// The == operator will compare for equality after doing any necessary type conversions. 
+// The === operator will not do the conversion, so if two values are not the same type === will simply return false. 
+
+// Both are equally quick.
+
+
+
---
+++ .devcontainer/js_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+//  Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+const score = 100
+const scoreValue = 100.3
+console.log(typeof score)
+// const isLoggedIn = false 
+// const outsideTemp = null
+// let userEmail;
+
+// const id =Symbol('123')
+// const anotherId =Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 111111114444444444555555555n
+// console.log(bigNumber);
+
+
+// Javascipt is a dynamically typed language.
+
+// Reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "mandj", "lolu"];
+console.log(typeof heros);
+
+let myObj = {
+    name: "ritik",
+    age: 23,
+}
+console.log(typeof myObj);
+
+const myFunction = function () {
+    console.log("hello world");
+}
+
